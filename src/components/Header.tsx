194dbd1b3d648170064e5093d5d6ac95ec4e8837
Etom <98264570+EtomCoda@users.noreply.github.com>
2025-05-07 10:00:17 -0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, X, Moon, Sun } from 'lucide-react';
 import { ThemeMode } from '../types';
+import logo from 'src/img/metransparent.png';
 interface HeaderProps {
   theme: ThemeMode;
   toggleTheme: () => void;
@@ -40,8 +41,8 @@ const Header: React.FC<HeaderProps> = ({ theme, toggleTheme }) => {
         : 'bg-transparent py-6'
     }`}>
       <div className="container mx-auto px-4 flex justify-between items-center">
+         <img src={logo} alt="MyLogo" className="inline-block h-8 w-8 mr-2" />
         <div className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-          <img src="src/img/metransparent.png" alt="Logo" className="inline-block h-8 w-8 mr-2" />
           <span className="text-blue-600 dark:text-blue-400">Meet</span>Me
         </div>
 
