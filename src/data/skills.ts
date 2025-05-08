8ab4780b482b78a4ae03c41dd0361d05ccf50c78
Etom <98264570+EtomCoda@users.noreply.github.com>
2025-05-07 18:06:36 -0700

    Update skills.ts

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,18 +1,18 @@
 import { Skill } from '../types';
 
 export const skills: Skill[] = [
-  { name: 'NestJS', level: 5, category: 'backend' },
-  { name: 'TypeScript', level: 5, category: 'backend' },
+  { name: 'NestJS', level: 3, category: 'backend' },
+  { name: 'TypeScript', level: 4, category: 'backend' },
   { name: 'Node.js', level: 5, category: 'backend' },
   { name: 'Express', level: 4, category: 'backend' },
   { name: 'RESTful APIs', level: 5, category: 'backend' },
   
   { name: 'PostgreSQL', level: 4, category: 'database' },
-  { name: 'MongoDB', level: 4, category: 'database' },
+  { name: 'MongoDB', level: 3, category: 'database' },
   
   
-  { name: 'CI/CD', level: 4, category: 'devops' },
+  { name: 'CI/CD', level: 3, category: 'devops' },
  
   { name: 'React', level: 3, category: 'frontend' },
-  { name: 'HTML/CSS', level: 3, category: 'frontend' },
+  { name: 'HTML/CSS', level: 5, category: 'frontend' },
 ];
